c16b7ce18c1003578be44b78f7d9a04c1a53f4ff
edoomm <edoomm8@gmail.com>
2021-07-19 14:54:42 -0500

    exportation changed
    csv file instead of sql file

--- web/sys/export/index.php
+++ web/sys/export/index.php
@@ -1,80 +1,55 @@
 <?php 
     include '../../database/evaseacdb.php';
+    
+    $content = '';
+    $table_names = array("parametroscrudo", "parametros", "imagenes", "parametrospsitios", "parametrosp", "prueba", "generossitios", "genero", "familiassitios", "familia",
+        "orden", "clase", "temporada", "sitio", "proyecto", "publicacion", "miembro", "area", "usuario");
+    $conn = open_database();
+    if (!$conn)
+        die("Error al conectar al servidor: " . mysqli_connect_error());
 
-   //or add 5th parameter(array) of specific tables:    array("mytable1","mytable2","mytable3") for multiple tables
+    foreach ($table_names as $table) {
+        // table names
+        $content .= "-" . $table . "-\n";
 
-    Export_Database(SQL_HOST, SQL_USER, SQL_PWD, SQL_DB, $tables=false, $backup_name=false);
-
-    function Export_Database($host,$user,$pass,$name,  $tables=false, $backup_name=false )
-    {
-        $mysqli = new mysqli($host,$user,$pass,$name); 
-        $mysqli->select_db($name); 
-        $mysqli->query("SET NAMES 'utf8'");
-
-        $queryTables    = $mysqli->query('SHOW TABLES'); 
-        while($row = $queryTables->fetch_row()) 
-        { 
-            $target_tables[] = $row[0]; 
-        }   
-        if($tables !== false) 
-        { 
-            $target_tables = array_intersect( $target_tables, $tables); 
+        // table info
+        // column names
+        $result = mysqli_query($conn, "SHOW COLUMNS FROM " . $table);
+        if($result) {
+            while ($row = mysqli_fetch_array($result)) {
+                $content .= $row['Field'] . ",";
+            }
+            $content = rtrim($content, ", ");
+            $content .= "\n";
         }
-        foreach($target_tables as $table)
-        {
-            $result         =   $mysqli->query('SELECT * FROM '.$table);  
-            $fields_amount  =   $result->field_count;  
-            $rows_num=$mysqli->affected_rows;     
-            $res            =   $mysqli->query('SHOW CREATE TABLE '.$table); 
-            $TableMLine     =   $res->fetch_row();
-            $content        = (!isset($content) ?  '' : $content) . "\n\n".$TableMLine[1].";\n\n";
-
-            for ($i = 0, $st_counter = 0; $i < $fields_amount;   $i++, $st_counter=0) 
-            {
-                while($row = $result->fetch_row())  
-                { //when started (and every after 100 command cycle):
-                    if ($st_counter%100 == 0 || $st_counter == 0 )  
-                    {
-                            $content .= "\nINSERT INTO ".$table." VALUES";
+        // table data
+        $query = "SELECT * FROM " . $table;
+        $result = mysqli_query($conn, $query);
+        if ($result) {
+            while ($row = mysqli_fetch_array($result)) {
+                foreach ($row as $key => $value) {
+                    if (is_int($key)) {
+                        $content .= get_sql_value($value) . ",";
                     }
-                    $content .= "\n(";
-                    for($j=0; $j<$fields_amount; $j++)  
-                    { 
-                        $row[$j] = str_replace("\n","\\n", addslashes($row[$j]) ); 
-                        if (isset($row[$j]))
-                        {
-                            $content .= '"'.$row[$j].'"' ; 
-                        }
-                        else 
-                        {   
-                            $content .= '""';
-                        }     
-                        if ($j<($fields_amount-1))
-                        {
-                                $content.= ',';
-                        }      
-                    }
-                    $content .=")";
-                    //every after 100 command cycle [or at last line] ....p.s. but should be inserted 1 cycle eariler
-                    if ( (($st_counter+1)%100==0 && $st_counter!=0) || $st_counter+1==$rows_num) 
-                    {   
-                        $content .= ";";
-                    } 
-                    else 
-                    {
-                        $content .= ",";
-                    } 
-                    $st_counter=$st_counter+1;
                 }
-            } $content .="\n\n\n";
+                // // $content .= $row[0];
+                // foreach ($row as $element) {
+                //     $content .= $element . ",";
+                // }
+                $content = rtrim($content, ", ");
+                $content .= "\n";
+            }
         }
-        //$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
-        $backup_name = $backup_name ? $backup_name : $name.".sql";
-        header('Content-Type: application/octet-stream');   
-        header("Content-Transfer-Encoding: Binary"); 
-        header("Content-disposition: attachment; filename=\"".$backup_name."\"");  
-        echo $content; exit;
     }
 
-    header("../../");
+    // echo $content;
+
+    // downloads 
+    $file_name = date("Y-m-d") . "-ev.csv";
+    header('Content-Type: application/octet-stream');   
+    header("Content-Transfer-Encoding: Binary"); 
+    header("Content-disposition: attachment; filename=\"".$file_name."\"");  
+    echo $content; exit;
+
+    header("../");
 ?>
